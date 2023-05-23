570c5e187123b13f025e2e1016e479af71f7e4d1
Miguel Tavares <miguel.tavares07@ua.pt>
2023-05-23 15:44:19 +0100

    Ir escolhendo os centrois em cada interação

--- obus/test.py
+++ obus/test.py
@@ -1,53 +1,124 @@
-import numpy as np
+import numpy as np, threading, time, random
 import matplotlib.pyplot as plt
+import geopandas as gpd
+from shapely.geometry import Polygon, LineString, Point
+from geographiclib.geodesic import Geodesic
 
-# Define the zone coordinates
-zone = [(37.87694, -25.78116), (37.87091, -25.77317),
-        (37.8703, -25.78948), (37.86342, -25.78214)]
+def find_closest_point(current_position, points):
+    return min(points, key=lambda vertex: calculate_distance(current_position, vertex))
 
-# Rearrange the points to form a rectangle
-sorted_zone = sorted(zone, key=lambda x: x[0]) # Sort points by latitude
-bottom_points = sorted(sorted_zone[:2], key=lambda x: x[1]) # Sort the bottom two points by longitude
-top_points = sorted(sorted_zone[2:], key=lambda x: x[1]) # Sort the top two points by longitude
-rectangle_points = bottom_points + top_points[::-1] + [bottom_points[0]] # Create a closed rectangle path
+def calculate_distance(point1, point2):
+    return Geodesic.WGS84.Inverse(point1[0], point1[1], point2[0], point2[1])['s12']
 
-# Find the bounding box (min and max latitudes and longitudes)
-min_lat = min([point[0] for point in rectangle_points])
-max_lat = max([point[0] for point in rectangle_points])
-min_lon = min([point[1] for point in rectangle_points])
-max_lon = max([point[1] for point in rectangle_points])
+def reorder_points(points):
+    min_lat_point = min(points, key=lambda x: x[0])
+    max_lat_point = max(points, key=lambda x: x[0])
+    
+    points.remove(min_lat_point)
+    points.remove(max_lat_point)
 
-# Define the number of cells you want to divide the area into
-n_lat = 10
-n_lon = 10
+    min_lon_point = min(points, key=lambda x: x[1])
+    max_lon_point = max(points, key=lambda x: x[1])
 
-# Create the grid lines for latitude and longitude
-lat_lines = np.linspace(min_lat, max_lat, n_lat + 1)
-lon_lines = np.linspace(min_lon, max_lon, n_lon + 1)
+    return [min_lat_point, min_lon_point, max_lat_point, max_lon_point]
 
-# Calculate the midpoints for latitude and longitude
-lat_midpoints = (lat_lines[1:] + lat_lines[:-1]) / 2
-lon_midpoints = (lon_lines[1:] + lon_lines[:-1]) / 2
+def create_grid(zone, resolution, zone_polygon):
+    min_lat, min_lon = np.inf, np.inf
+    max_lat, max_lon = -np.inf, -np.inf
 
-# Plot the grid
-fig, ax = plt.subplots()
-ax.set_xlim(min_lon, max_lon)
-ax.set_ylim(min_lat, max_lat)
+    for point in zone:
+        lat, lon = point
+        min_lat = min(min_lat, lat)
+        max_lat = max(max_lat, lat)
+        min_lon = min(min_lon, lon)
+        max_lon = max(max_lon, lon)
 
-for lat in lat_lines:
-    ax.plot([min_lon, max_lon], [lat, lat], 'k-')
+    lat_step = (max_lat - min_lat) / resolution
+    lon_step = (max_lon - min_lon) / resolution
 
-for lon in lon_lines:
-    ax.plot([lon, lon], [min_lat, max_lat], 'k-')
+    lat_range = [min_lat + i * lat_step for i in range(resolution + 1)]
+    lon_range = [min_lon + i * lon_step for i in range(resolution + 1)]
 
-# Create a rectangle (polygon) using the rearranged points
-rectangle_points_array = np.array(rectangle_points)
+    grid = []
+    centroids = []
 
-ax.fill(rectangle_points_array[:, 1], rectangle_points_array[:, 0], 'c', alpha=0.5)
+    for i in range(resolution):
+        for j in range(resolution):
+            cell = [(lat_range[i], lon_range[j]), 
+                    (lat_range[i + 1], lon_range[j]), 
+                    (lat_range[i + 1], lon_range[j + 1]), 
+                    (lat_range[i], lon_range[j + 1]),
+                    (lat_range[i], lon_range[j])]
 
-# Plot the middle point for each cell
-for lat in lat_midpoints:
-    for lon in lon_midpoints:
-        ax.plot(lon, lat, 'bo')
+            cell_polygon = Polygon(cell)
+            centroid = cell_polygon.centroid
+            if not zone_polygon.contains(centroid):
+                sub_resolution = 10
+                sub_lat_step = lat_step / sub_resolution
+                sub_lon_step = lon_step / sub_resolution
 
-plt.show()
+                flag = False
+                for m in range(sub_resolution):
+                    for n in range(sub_resolution):
+                        sub_cell = [(lat_range[i] + m * sub_lat_step, lon_range[j] + n * sub_lon_step),
+                                    (lat_range[i] + (m + 1) * sub_lat_step, lon_range[j] + n * sub_lon_step),
+                                    (lat_range[i] + (m + 1) * sub_lat_step, lon_range[j] + (n + 1) * sub_lon_step),
+                                    (lat_range[i] + m * sub_lat_step, lon_range[j] + (n + 1) * sub_lon_step),
+                                    (lat_range[i] + m * sub_lat_step, lon_range[j] + n * sub_lon_step)]
+                        sub_cell_polygon = Polygon(sub_cell)
+                        sub_centroid = sub_cell_polygon.centroid
+                        if zone_polygon.contains(sub_centroid):
+                            grid.append(cell)
+                            centroids.append((sub_centroid.x,sub_centroid.y))
+                            flag = True
+                            break
+                    if flag:
+                        break
+            else:
+                grid.append(cell)
+                centroids.append((centroid.x,centroid.y))
+
+    return grid, centroids
+
+def find_closest_unclaimed_cell(device_id, current_position, cells, claimed_cells):
+    unclaimed_cells = [c for c in cells if c not in claimed_cells]
+    if not unclaimed_cells:
+        return None
+
+    closest_cell = find_closest_point(current_position, unclaimed_cells)
+    claimed_cells.append(closest_cell)
+    print(f"Device {device_id} claimed cell {closest_cell}")
+    return closest_cell
+
+def device_routine(device_id, start_position, cells, claimed_cells):
+    current_position = start_position
+    path = [Point(start_position)]
+    while True:
+        closest_cell = find_closest_unclaimed_cell(device_id, current_position, cells, claimed_cells)
+        if closest_cell is None:
+            print(f"Device {device_id} finished patrolling")
+            break
+
+        # Simulate the time it takes for the device to move to the new cell
+        time.sleep(random.uniform(0.1, 0.5))
+        current_position = closest_cell
+
+
+zone = reorder_points([(37.87694, -25.78116), (37.87091, -25.77317),
+        (37.8703, -25.78948), (37.86342, -25.78214)])
+
+resolution = 10
+num_devices = 3
+zone_polygon = Polygon(zone)
+grid, centroids = create_grid(zone, resolution, zone_polygon)
+
+# Assume your cells and their centroids are produced by create_grid
+grid, centroids = create_grid(zone, resolution, zone_polygon)
+
+# Assume you have a list of starting positions for the devices
+device_positions = [(37.87091, -25.77317), (37.87694, -25.78116)]
+
+claimed_cells = []
+for i, position in enumerate(device_positions):
+    threading.Thread(target=device_routine, args=(i, position, centroids, claimed_cells)).start()
+
